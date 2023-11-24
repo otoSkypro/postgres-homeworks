4459999f6a0883caf9b962e700e949971171c782
Oto <140820086+otoSkypro@users.noreply.github.com>
2023-11-24 12:23:24 +0300

    Update alter_student.sql

--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,42 +1,35 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
-CREATE TABLE student
-(
-	student_id serial,
-	first_name varchar,
-	last_name varchar,
-	birthday date,
-	phone varchar,
-
-	CONSTRAINT pk_student_student_id PRIMARY KEY (student_id)
-)
+CREATE TABLE student (
+    student_id serial,
+    first_name varchar,
+    last_name varchar,
+    birthday date,
+    phone varchar
+);
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
-ALTER TABLE student ADD COLUMN middle_name varchar
-
+ALTER TABLE student
+ADD COLUMN middle_name varchar;
 
 -- 3. Удалить колонку middle_name
-
-ALTER TABLE student DROP COLUMN middle_name
-
+ALTER TABLE student
+DROP COLUMN middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
-ALTER TABLE student RENAME COLUMN birthday TO birth_day
-
+ALTER TABLE student
+RENAME COLUMN birthday TO birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
-ALTER TABLE student ALTER COLUMN phone SET DATA TYPE varchar(32)
-
+ALTER TABLE student
+ALTER COLUMN phone TYPE varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
-INSERT INTO student (first_name, last_name, phone) VALUES ('Павлов', 'Дмитрий', '+77777777777');
-INSERT INTO student (first_name, last_name, birth_day) VALUES ('Баринова', 'Анна', '01-12-1992'), ('Губин', 'Андрей', '07-03-1978')
-
+INSERT INTO student (first_name, last_name, birth_date, phone)
+VALUES
+    ('John', 'Doe', '1990-01-01', '123-456-7890'),
+    ('Alice', 'Smith', '1995-05-15', '987-654-3210'),
+    ('Bob', 'Johnson', '1988-12-10', '555-123-4567');
 
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
-
-TRUNCATE TABLE student RESTART IDENTITY
+DELETE FROM student;
+ALTER SEQUENCE student_student_id_seq RESTART WITH 1;
